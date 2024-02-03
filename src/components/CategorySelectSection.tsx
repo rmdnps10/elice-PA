1a9a5f7701b826d77837005a8ea31f69b200908d
inyoung <rmdnps10@gmail.com>
2024-02-04 05:28:20 +0900

    Style: FilterBox 반응형에 따라 스타일 무너지지 않게 디자인

--- src/components/CategorySelectSection.tsx
+++ src/components/CategorySelectSection.tsx
@@ -2,32 +2,9 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Space from 'util/Space';
+import { filterListByCategory } from 'util/filterListByCategory';
 
 function CategorySelectSection() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(
-        'https://api-rest.elice.io/org/academy/course/list/',
-        {
-          params: {
-            offset: 0,
-            count: 20,
-            and: [
-              { title: 'c언어' },
-              {
-                or: [
-                  { enroll_type: 0, is_free: true },
-                  { enroll_type: 0, is_free: false },
-                ],
-              },
-            ],
-          },
-        },
-      );
-      console.log(res);
-    };
-    fetchData();
-  }, []);
   return (
     <>
       <Space height="6.4rem" />
@@ -36,6 +13,56 @@ function CategorySelectSection() {
         <ImpactMessage>
           엘리스에서 배우고자 하는 과목의 특성을 선택해주세요.
         </ImpactMessage>
+        <FilterBox>
+          <FilterBoxRow>
+            <FilterCategory>유형</FilterCategory>
+            <FilterList>
+              {filterListByCategory?.courseType.map((item) => (
+                <FilterItem key={item}>{item}</FilterItem>
+              ))}
+            </FilterList>
+          </FilterBoxRow>
+          <FilterBoxRow>
+            <FilterCategory>진행방식</FilterCategory>
+            <FilterList>
+              {filterListByCategory?.format.map((item) => (
+                <FilterItem key={item}>{item}</FilterItem>
+              ))}
+            </FilterList>
+          </FilterBoxRow>
+          <FilterBoxRow>
+            <FilterCategory>분야</FilterCategory>
+            <FilterList>
+              {filterListByCategory?.category.map((item) => (
+                <FilterItem key={item}>{item}</FilterItem>
+              ))}
+            </FilterList>
+          </FilterBoxRow>
+          <FilterBoxRow>
+            <FilterCategory>난이도</FilterCategory>
+            <FilterList>
+              {filterListByCategory?.level.map((item) => (
+                <FilterItem key={item}>{item}</FilterItem>
+              ))}
+            </FilterList>
+          </FilterBoxRow>
+          <FilterBoxRow>
+            <FilterCategory>언어</FilterCategory>
+            <FilterList>
+              {filterListByCategory?.programmingLanguage.map((item) => (
+                <FilterItem key={item}>{item}</FilterItem>
+              ))}
+            </FilterList>
+          </FilterBoxRow>
+          <FilterBoxRow>
+            <FilterCategory>가격</FilterCategory>
+            <FilterList>
+              {filterListByCategory?.price.map((item) => (
+                <FilterItem key={item}>{item}</FilterItem>
+              ))}
+            </FilterList>
+          </FilterBoxRow>
+        </FilterBox>
       </CategorySelectSectionWrapper>
     </>
   );
@@ -60,6 +87,47 @@ const ImpactMessage = styled.div`
   font-size: 1.8rem;
   font-weight: 400;
   color: #7d7e80;
+`;
+
+const FilterBox = styled.div`
+  border: 1px solid rgb(225, 226, 228);
+`;
+
+const FilterBoxRow = styled.div`
+  display: flex;
+  border-bottom: 1px solid rgb(225, 226, 228);
+`;
+
+const FilterCategory = styled.div`
+  font-size: 1.2rem;
+  line-height: 150%;
+  padding: 1.4rem 1.6rem;
+  background: #f9fafc;
+  border-right: 1px solid rgb(225, 226, 228);
+  color: #5e5f61;
+  min-width: 6rem;
+  font-weight: 600;
+`;
+
+const FilterList = styled.div`
+  flex-grow: 1;
+  padding: 0 0.5rem;
+  display: flex;
+  background: white;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const FilterItem = styled.div`
+  padding: 0.4rem 1.2rem;
+  font-size: 1.4rem;
+  margin: 0.8rem;
+  line-height: 150%;
+  font-weight: 400;
+  border-radius: 1.875rem;
+  background: #f0f1f3;
+  cursor: pointer;
+  color: #5e5f61;
 `;
 
 export default CategorySelectSection;
--- src/util/filterListByCategory.ts
+++ src/util/filterListByCategory.ts
@@ -1,26 +1,22 @@
-export const courseType = ['과목', '챌린지', '테스트'];
-export const format = ['자유 선택형', '순차 완료형'];
-export const category = [
-  '프로그래밍 기초',
-  '데이터 분석',
-  '웹',
-  '인공지능',
-  '알고리즘',
-];
-
-export const level = ['입문', '초급', '중급', '고급', '심화'];
-export const programmingLanguage = [
-  'C',
-  'C++',
-  '자바',
-  '파이썬',
-  '자바스크립트',
-  'R',
-  'HTML/CSS',
-  'SQL',
-  '아두이노',
-  '스크래치',
-  '코틀린',
-  '스위프트',
-  '엔트리',
-];
+export const filterListByCategory = {
+  courseType: ['과목', '챌린지', '테스트'],
+  format: ['자유 선택형', '순차 완료형'],
+  category: ['프로그래밍 기초', '데이터 분석', '웹', '인공지능', '알고리즘'],
+  level: ['입문', '초급', '중급', '고급', '심화'],
+  programmingLanguage: [
+    'C',
+    'C++',
+    '자바',
+    '파이썬',
+    '자바스크립트',
+    'R',
+    'HTML/CSS',
+    'SQL',
+    '아두이노',
+    '스크래치',
+    '코틀린',
+    '스위프트',
+    '엔트리',
+  ],
+  price: ['무료', '유료', '구독', '학점'],
+};
